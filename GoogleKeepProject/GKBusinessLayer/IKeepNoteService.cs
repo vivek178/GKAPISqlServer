190d0e1587deebc733019a153eef888c4ea462ba
vivek178 <vpyasi75@gmail.com>
2019-08-07 16:37:37 +0530

    updated some files.

--- GoogleKeepProject/GKBusinessLayer/IKeepNoteService.cs
+++ GoogleKeepProject/GKBusinessLayer/IKeepNoteService.cs
@@ -1,21 +1,20 @@
 ﻿using Entities;
+using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace GKBusinessLayer
 {
-    /// <summary>
-    /// Interface for Accessing the properties of Notes Class.
-    /// </summary>
     public interface IKeepNoteService
     {
         int CreateNote(Notes notes);
 
-        int RemoveNote(int NoteID);
-
-        int UpdateNote(Notes notes);
+        List<Notes> GetAllNotes();
 
         Notes GetNotesByID(int NoteID);
 
-        List<Notes> GetAllNotes();
+        int RemoveNote(int NoteID);
+
+        int UpdateNote(Notes notes);
     }
 }
